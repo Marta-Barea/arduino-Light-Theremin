313de9d03491fa7b1a2b84d81a058be40b2a8600
Marta Barea-Sepúlveda <79462299+Marta-Barea@users.noreply.github.com>
2022-05-29 18:57:55 +0200

    Updated LightTheremin.ino

--- LightTheremin.ino
+++ LightTheremin.ino
@@ -15,43 +15,47 @@ const int ledPin = 13;
 
 // Set on the specified pin whether it should behave as an input or an output:
 
-void setup() {
+void setup()
+{
 
- pinMode(ledPin, OUTPUT); // Make the LED pin an output
- digitalWrite(ledPin, HIGH); // Turn the LED pin on
+  pinMode(ledPin, OUTPUT);    // Make the LED pin an output
+  digitalWrite(ledPin, HIGH); // Turn the LED pin on
 
- // Calibrate the sensor minimums and maximums:
+  // Calibrate the sensor minimums and maximums:
 
- while(millis() < 5000) {
+  while (millis() < 5000)
+  {
 
-  sensorValue = analogRead(A0);
-  
-  // Record the maximum sensor value:
-  
-  if (sensorValue > sensorHigh) {
-    sensorHigh = sensorValue;
+    sensorValue = analogRead(A0);
+
+    // Record the maximum sensor value:
+
+    if (sensorValue > sensorHigh)
+    {
+      sensorHigh = sensorValue;
+    }
+
+    // Record the minimun sensor value:
+
+    if (sensorValue > sensorLow)
+    {
+      sensorLow = sensorValue;
+    }
   }
 
-  // Record the minimun sensor value:
-  
-  if(sensorValue > sensorLow) {
-    sensorLow = sensorValue;
-  }
- 
- }
-
- // Turn the LED off, indicating the end of the calibration period:
+  // Turn the LED off, indicating the end of the calibration period:
 
   digitalWrite(ledPin, LOW);
 }
 
 // The loop routine that runs on and on:
 
-void loop() {
+void loop()
+{
 
   sensorValue = analogRead(A0); // Read the input from the photoresistor and store it in a varible
 
-  // Declares a variable to store the mapping of sensor values:
+  // Declare a variable to store the mapping of sensor values:
 
   int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
 
@@ -60,6 +64,6 @@ void loop() {
   tone(8, pitch, 20);
 
   // Delay between reads:
-  
+
   delay(10);
 }
